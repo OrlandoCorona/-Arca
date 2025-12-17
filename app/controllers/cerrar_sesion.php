7aacde12c93c1cd61f2a989700e53ef0e35d911f
Orlando1221 <carlosorlacorona@gmail.com>
2025-12-17 15:03:03 -0600

    fix cerrar sesion usando router

--- app/controllers/cerrar_sesion.php
+++ app/controllers/cerrar_sesion.php
@@ -1,6 +1,10 @@
 <?php
-// Cerrar sesión y redirigir a la página de inicio de sesión
-// Aquí debes agregar la lógica para cerrar la sesión, por ejemplo, eliminar las variables de sesión, etc.
-header("Location: index.html");
+session_start();
+
+/* Destruir la sesión */
+$_SESSION = [];
+session_destroy();
+
+/* Redirigir usando el router */
+header("Location: /?view=login");
 exit();
-?>
