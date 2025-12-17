494d9b2c3ddb941b9d139682cd1cab7889e33796
Orlando1221 <carlosorlacorona@gmail.com>
2025-12-17 11:35:03 -0600

    refactor: standardize view naming to english

--- public/index.php
+++ public/index.php
@@ -3,4 +3,19 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../app/views/auth/login.php';
+$view = $_GET['view'] ?? 'login';
+
+$routes = [
+    'login'    => __DIR__ . '/../app/views/auth/login.php',
+    'register' => __DIR__ . '/../app/views/registro.html',
+    'recover'  => __DIR__ . '/../app/views/recuperar_contrasena.html',
+    'menu'     => __DIR__ . '/../app/views/menu.html',
+];
+
+if (!array_key_exists($view, $routes)) {
+    http_response_code(404);
+    echo 'Página no encontrada';
+    exit;
+}
+
+require $routes[$view];
