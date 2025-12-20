64c7c83c6e8fc89318e1a00660c67f67d0eea1cb
Orlando1221 <carlosorlacorona@gmail.com>
2025-12-20 13:59:55 -0600

    Lo logreee

--- app/controllers/cerrar_sesion.php
+++ app/controllers/cerrar_sesion.php
@@ -1,23 +1,7 @@
 <?php
 declare(strict_types=1);
 
-// La sesión ya está iniciada en public/index.php
-
-$_SESSION = [];
-
-if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params['path'],
-        $params['domain'],
-        $params['secure'],
-        $params['httponly']
-    );
-}
-
+session_unset();
 session_destroy();
 
 header('Location: /?view=login');
