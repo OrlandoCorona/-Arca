39f15c7d436954a67c5ec7d7e11b87b3a0b8a299
Orlando1221 <carlosorlacorona@gmail.com>
2025-12-20 07:28:51 -0600

    Yaaaaa Casii

--- public/index.php
+++ public/index.php
@@ -3,38 +3,10 @@ declare(strict_types=1);
 
 /**
  * Front Controller
+ * - Inicia sesión
+ * - Delega TODO el control a routes.php
  */
+
 session_start();
 
 require __DIR__ . '/../app/routes.php';
-
-/**
- * Detectar acción o vista
- */
-$action = $_GET['action'] ?? null;
-$view   = $_GET['view'] ?? null;
-
-/**
- * Acciones (POST)
- */
-if ($action && isset($actionsMap[$action])) {
-    require $actionsMap[$action];
-    exit;
-}
-
-/**
- * Vistas (GET)
- */
-if ($view && isset($viewsMap[$view])) {
-    require $viewsMap[$view];
-    exit;
-}
-
-/**
- * Default
- */
-require $viewsMap['login'];
-        
-
-
-exit;
